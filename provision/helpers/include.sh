ef47802dff1c74c980019cc499b366c183a6b2f4
Dennis Nygren <dennis@dreamhuman.com>
2019-01-02 16:46:16 -0800

    Change helper function colors.sh to text.sh

--- provision/helpers/include.sh
+++ provision/helpers/include.sh
@@ -6,5 +6,5 @@ SHOW_COMMAND=true
 SHOW_COMMAND_OUTPUT=false
 SHOW_COMMAND_EXECUTION_TIME=true
 
-. /vagrant/provision/helpers/colors.sh
+. /vagrant/provision/helpers/text.sh
 . /vagrant/provision/helpers/functions.sh

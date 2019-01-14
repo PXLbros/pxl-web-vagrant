2af4d1df966982162f9f997048812ef208bf6347
Dennis Nygren <dennis@dreamhuman.com>
2019-01-14 00:25:32 -0800

    Better install helper

--- scripts/sites/classes/install_helper.js
+++ scripts/sites/classes/install_helper.js
@@ -1,16 +1,39 @@
-const { exec } = require('shelljs');
+const { existsSync } = require('fs');
+const { cp, exec } = require('shelljs');
 
 class InstallHelper
 {
     constructor(pxl_config) {
         this.pxl_config = pxl_config;
+
+        this.php_cli = `php${this.pxl_config.code.php}`;
     }
 
     install() {
     }
 
+    php(command) {
+        exec(`${this.php_cli} ${command}`);
+    }
+
     composer(command) {
-        exec(`php${this.pxl_config.code.php} /usr/local/bin/composer ${command}`);
+        this.php(`/usr/local/bin/composer ${command}`);
+    }
+
+    file_exists(path) {
+        return existsSync(path);
+    }
+
+    copy_file(from, to) {
+        cp(from, to);
+    }
+
+    edit_file_line_pre(file, pre, value) {
+        exec(`sed -i s~${pre}.*$~${pre}${value}~g ${file}`);
+    }
+
+    yarn(command = null) {
+        exec(`yarn${command !== null ? ` ${command}` : ``}`);
     }
 }
 
